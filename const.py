8194a25e9315c6146ae3cb68499078a234260e38
Wilds <ivoselva@gmail.com>
2025-10-21 16:00:06 +0200

    Add Meari partner details to KNOWN_PARTNERS

--- const.py
+++ const.py
@@ -28,4 +28,17 @@ KNOWN_PARTNERS: dict = {
         'partner_secret': 'aTCOXp79xCGLFdb1HfBAPgXHnoVf9WlZ',
 
     },
+    'meari': {
+        'name': 'Meari',
+        'url': 'https://www.meari.com/',
+
+        'init_type': '1',    # TODO
+
+        'app_version': '5.10.4',
+        'app_version_code': '5104',
+        'source_app': '81',  # TODO
+
+        'partner_key': 'e84740f8629245b5892fab115c6d08aa',
+        'partner_secret': '1ed87fee47984baba5326190dfff8ab0',
+    }
 }
